51fc7532116adeffcaea5835ad3f0286f5c32d64
Nicholas Rynearson <nrynearson@gmail.com>
2021-09-22 16:06:10 -0400

    creates some divs in basic shapes to play with

--- animations/src/screens/Home/Home.jsx
+++ animations/src/screens/Home/Home.jsx
@@ -5,12 +5,12 @@ import "./Home.css"
 function Home(){
   const [animate, setAnimate] = useState(false)
   useEffect(()=> {
-    setAnimate(true)
+    setAnimate(false)
   }, [])
   return(
     <div className="home">
-      <h2>this is the homepage</h2>
       <div className={ animate ? "box grow" : "box"}></div>
+      <div className="circle"></div>
     </div>
   )
 }
